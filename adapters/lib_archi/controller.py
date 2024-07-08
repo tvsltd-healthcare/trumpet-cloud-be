37b345d946481e94c2ed4d5458e8f052305f4b4a
Nur <nur@technovativesolutions.co.uk>
2024-07-08 17:45:48 +0600

    fix(controller): resolve MRO conflict for generic Controller class

--- adapters/lib_archi/controller.py
+++ adapters/lib_archi/controller.py
@@ -6,9 +6,9 @@ from lib_archi.base_application_service import BaseApplicationService
 Entity = TypeVar('Entity')
 
 
-class Controller(IController):
-    def __init__(self, appService: BaseApplicationService[Entity]):
-        self.controller = BaseController(appService)
+class Controller(IController, Generic[Entity]):
+    def __init__(self, apps_service: BaseApplicationService[Entity]):
+        self.controller = BaseController(apps_service)
 
     def post(self, entity: Entity):
         return self.controller.post(entity.id, entity)
