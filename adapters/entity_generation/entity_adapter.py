b5ecc0b9c66ca62dde21480fc85555661a297947
Nur <nur@technovativesolutions.co.uk>
2024-10-21 15:11:46 +0600

    feat: implement middleware

--- adapters/entity_generation/entity_adapter.py
+++ adapters/entity_generation/entity_adapter.py
@@ -1,11 +1,14 @@
 from typing import Dict
 
 from application_layer.abstractions.entity_interface import IEntityGenerator
-
+from wrap_validate import EntryPoint
 
 class EntityAdapter(IEntityGenerator):
-    def __init__(self, entity_adapter):
-        self.entity = entity_adapter
+    def __init__(self):
+        self.entity = EntryPoint()
 
     def create(self, entity_name: str, input_dict: Dict):
         return self.entity.loader.load_schema(name=entity_name, schema=input_dict)
+
+    def validate(self, entity_name: str, data: Dict):
+        return self.entity.validator.validate(schema=entity_name, data=data)
--- application_layer/entities/__init__.py
+++ application_layer/entities/__init__.py
@@ -1,9 +1,17 @@
+from typing import Any, Dict
+
 from .product import Product
 from .user import User
 from .organizations import Organizations
 
-from typing import Any, Dict
+from adapters.entity_generation.entity_adapter import EntityAdapter
+
+
+entity_adapter_obj = EntityAdapter()
 
 
 def get_resource_types() -> Dict[str, Any]:
-    return {"products": Product, "users": User, "organizations": Organizations}
+    _user = entity_adapter_obj.create(entity_name='Users', input_dict=User)
+    _organizations = entity_adapter_obj.create(entity_name='Organizations', input_dict=Organizations)
+
+    return {"users": _user, "organizations": _organizations}
