15db680be55c90119b1b3c9569fd8bde321cae32
khaled-imran <khaled.imran@technovativesolutions.co.uk>
2024-12-10 21:42:17 +0600

    fix: modified injector logic for mapping study agreement

--- logic_injector/base_logic_injector.py
+++ logic_injector/base_logic_injector.py
@@ -15,7 +15,8 @@ AVAILABLE_DOS = os.getenv("DO_ENDPOINTS").split(",")
 class BaseLogicInjector:
     def __init__(self):
         pass
-    def inject_business_logic(self, entity_id: dict, entity: dict):
+
+    def inject_business_logic(self, entity_id: dict, entity: dict, agreement_id: int):
         # ToDo: generalize this for all the custom logics
         # Decode the dict from the entity
         entity= dict(entity)
@@ -25,6 +26,7 @@ class BaseLogicInjector:
         fl_injector_obj = FLSetupInjector()
 
         agg_response = fl_injector_obj.call_setup_on_agg_fl_core(fl_agg_core_url=os.getenv('TC_FL_CORE_BASE_URL'),
+                                                  agreement_id=agreement_id,
                                                   name=str(entity_id.get('study_id', "")),
                                                   coordinator=os.getenv('COORDINATOR'),
                                                   model=entity.get('model', "NN_FHIR"),
@@ -32,10 +34,11 @@ class BaseLogicInjector:
                                                   rounds=entity.get('rounds', "3"),
                                                   webhook_url=os.getenv('LISTENER_WEBHOOK_URL'),
                                                   participants=participants)
-
+        
         for do_endpoint in AVAILABLE_DOS:
             do_response = fl_injector_obj.call_setup_on_participants_do_fl_core(
                 do_url=do_endpoint,
+                agreement_id=agreement_id,
                 name=str(entity_id.get('study_id', "")),
                 coordinator=os.getenv('COORDINATOR'),
                 model=entity.get('model', "NN_FHIR"),
@@ -54,42 +57,45 @@ class FLSetupInjector:
     do_load_data_uri = "/fl-core/load-data/"
     do_setup_uri = "/fl-core/setup/"
 
-    def call_setup_on_agg_fl_core(self, fl_agg_core_url: str, name: str, coordinator: str, model: str, pet: str,
+    def call_setup_on_agg_fl_core(self, fl_agg_core_url: str, agreement_id: int, name: str, coordinator: str, model: str, pet: str,
                                  rounds: str,
                                  webhook_url: str, participants: List[str]) -> Union[bool, tuple[bool, dict]]:
         _request_body = {
             "name": name,
+            "study_id": str(agreement_id),
             "coordinator": coordinator,
             "model": model,
             "pet": pet,
+            "pet_config" : {},
             "rounds": rounds,
             "webhook_url": webhook_url,
-            "participants": participants,
+            "participants":  participants,
         }
 
         response = requests.post(url=fl_agg_core_url + self.setup_uri, json=_request_body)
+
         if response.status_code != 200:
             return False
         return True, response.json()
 
-    def call_setup_on_participants_do_fl_core(self, do_url: str, name: str, coordinator: str, model: str, pet: str,
+    def call_setup_on_participants_do_fl_core(self, do_url: str, agreement_id: int, name: str, coordinator: str, model: str, pet: str,
                                  rounds: str, description:str, purpose:str,
                                  webhook_url: str, participants: List[str]) -> Union[bool, tuple[bool, dict]]:
         _request_body = {
             "name": name,
+            "agreement_id": str(agreement_id),
             "coordinator": coordinator,
-            "description": description,
-            "purpose": purpose,
-            "status": "active",
             "model": model,
             "pet": pet,
+            "pet_config" : {},
             "rounds": rounds,
             "webhook_url": webhook_url,
-            "participants": participants,
+            "participants":  participants,
         }
 
         response = requests.post(url=do_url + self.do_setup_uri, json=_request_body)
-        if response.status_code != 200:
+
+        if (response.status_code != 200 or response.status_code != 201):
             return False
         return True, response.json()
 
