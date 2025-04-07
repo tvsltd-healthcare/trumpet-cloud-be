793259fa1da2f8d32e02de0a310519d94a69873e
Tajal <admin@admins-MacBook-Pro-3.local>
2025-04-07 13:39:09 +0600

    feat/verify_email

---
+++ domain_layer/logics/non_resources/email_logic.py
@@ -0,0 +1,34 @@
+
+import anyio
+
+from domain_layer.abstractions.app_repo_discovery_getter_interface import IAppRepoDiscoveryGetter
+from domain_layer.abstractions.app_repo_invoker_interface import IAppRepoInvoker
+from domain_layer.auth_manager import AuthManager
+from domain_layer.repo_discovery_manager import RepoDiscoveryManager
+
+
+def execute(request):
+    # todo: need to fix this implementation
+    # todo: the request implementation should come from wrap-restify
+    body = anyio.from_thread.run(request.json)
+    repo_discovery_getter_adapter: IAppRepoDiscoveryGetter = RepoDiscoveryManager.get()
+    user_repo_invoker: IAppRepoInvoker = repo_discovery_getter_adapter.get_repo_invoker("Users")
+    query = {
+        "email": body.get("email"),
+    }
+    user = user_repo_invoker.get(query, False)
+    if user:
+        auth_getter_adapter = AuthManager.get()
+        token = auth_getter_adapter.generate_token({"user_id": user.get('id')})
+        return {
+            "message": "User successfully logged in.",
+            "data": token,
+            "status_code": 200,
+        }
+    else:
+        return {
+            "message": "User not found",
+            "status_code": 404,
+        }
+
+
