ac0a74b58a035f42e18402cf3f86bc24877f7677
Tajal <admin@admins-MacBook-Pro-3.local>
2025-04-23 16:47:51 +0600

    feat: upload file

--- application_layer/entities/file.py
+++ application_layer/entities/file.py
@@ -33,6 +33,72 @@ Files = {
             'error_code': '5',
         },
     },
+    'path': {
+        'type': {
+            'value': 'string',
+            'error_code': '255',
+        },
+        'max': {
+            'value': 255,
+            'error_code': '256',
+        },
+        'required': {
+            'value': True,
+            'error_code': '5',
+        },
+    },
+    'filename': {
+        'type': {
+            'value': 'string',
+            'error_code': '10',
+        },
+        'max': {
+            'value': 255,
+            'error_code': '11',
+        },
+        'required': {
+            'value': True,
+            'error_code': '12',
+        },
+    },
+    'mime_type': {
+        'type': {
+            'value': 'string',
+            'error_code': '13',
+        },
+        'max': {
+            'value': 100,
+            'error_code': '14',
+        },
+        'required': {
+            'value': True,
+            'error_code': '15',
+        },
+    },
+    'size': {
+        'type': {
+            'value': 'number',
+            'error_code': '16',
+        },
+        'required': {
+            'value': True,
+            'error_code': '17',
+        },
+    },
+    'owner': {
+        'type': {
+            'value': 'string',
+            'error_code': '18',
+        },
+        'max': {
+            'value': 255,
+            'error_code': '19',
+        },
+        'required': {
+            'value': True,
+            'error_code': '20',
+        },
+    },
     'created_at': {
         'type': {
             'value': 'timestamp',
--- domain_layer/logics/non_resources/upload_logic.py
+++ domain_layer/logics/non_resources/upload_logic.py
@@ -1,25 +1,47 @@
-import os
 import anyio
-from datetime import datetime
-import pandas as pd
-
+from domain_layer.auth_manager import AuthManager
+from domain_layer.repo_discovery_manager import RepoDiscoveryManager
+from domain_layer.abstractions.app_repo_invoker_interface import IAppRepoInvoker
+from domain_layer.abstractions.app_repo_discovery_getter_interface import IAppRepoDiscoveryGetter
 
 def execute(request):
-    if request is None:
-        print("Request is None")
-        return {"message": "Invalid request", "status_code": 400}
     
     try:
-        print('===== Processing Request =====')
         body = anyio.from_thread.run(request.form)
-        file = body.get("file")
+        auth_header = request.headers['authorization']
 
+        # Remove "Bearer " prefix
+        token = auth_header.replace("Bearer ", "").strip()
+        
+        file = body.get("file")
         if file is None:
             return {"message": "No file provided", "status_code": 400}
 
-        print('=======file',file.filename)
-        
+        auth_getter_adapter = AuthManager.get()
+        decode_token = auth_getter_adapter.read_data(token)
 
+        repo_discovery_getter_adapter: IAppRepoDiscoveryGetter = RepoDiscoveryManager.get()
+        files_repo_invoker: IAppRepoInvoker = repo_discovery_getter_adapter.get_repo_invoker("Files")
+
+        create_files_body = {
+            "filename": file.filename,
+            "type": body.get("type"),
+            "buffer": body.get("buffer"),
+            "path":  f"{body.get('resource_name')}:{decode_token['user_id']}:{file.filename}",
+            "size": file.size,
+            "mime_type": file.headers.get('content-type'),
+            "owner": f"{body.get('resource_name')}:{decode_token['user_id']}",
+        }
+        
+        print('create_files_body=========>>>',create_files_body)
+        # create_file = files_repo_invoker.transact("POST", data =create_files_body)
+
+        return {
+            "message": "File created successfully.",
+            # "data": create_file,
+            "status_code": 200,
+        }
+        
     except Exception as e:
         print(f"Error: {e}")
         return {"message": str(e), "status_code": 500}
