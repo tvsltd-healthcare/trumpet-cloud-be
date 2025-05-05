45607340efe35372104de03dffe307fecc770a61
Sakib Nazmus <nsakib.cse@gmail.com>
2025-05-05 22:38:18 +0600

    Feat: Refactor file download logic to simplify and optimize imports
    Revised imports by removing unused modules and directly using `FileResponse` from Starlette. Changed request body parsing to query parameter extraction and added file path handling with a TODO for hardcoded directory path update.

--- domain_layer/logics/non_resources/file_download_logic.py
+++ domain_layer/logics/non_resources/file_download_logic.py
@@ -1,42 +1,32 @@
-from domain_layer import response_formatter
-from domain_layer.abstractions.request_interface import IRequest
-from domain_layer.utils.token_parser import token_parser
-from domain_layer.utils.file_upload import upload_file_to_disk
-from domain_layer.response_formatter import ResponseFormatter
-from domain_layer.repo_discovery_manager import RepoDiscoveryManager
-from domain_layer.utils.enforce_request_interface import enforce_request_type
-from domain_layer.abstractions.app_repo_invoker_interface import IAppRepoInvoker
+from starlette.responses import FileResponse
+
 from domain_layer.abstractions.app_repo_discovery_getter_interface import IAppRepoDiscoveryGetter
-from fastapi import FastAPI, Response # type: ignore
+from domain_layer.abstractions.app_repo_invoker_interface import IAppRepoInvoker
+from domain_layer.abstractions.request_interface import IRequest
+from domain_layer.repo_discovery_manager import RepoDiscoveryManager
+from domain_layer.response_formatter import ResponseFormatter
+from domain_layer.utils.enforce_request_interface import enforce_request_type
 
 
 @enforce_request_type()
 def execute(request: IRequest):
-    body = request.get_json()
+    body = request.get_query_params()
     file_id = body.get('file_id')
     response_formatter = ResponseFormatter()
 
-    # Manage repositary
     repo_discovery_getter: IAppRepoDiscoveryGetter = RepoDiscoveryManager.get()
     files_repo: IAppRepoInvoker = repo_discovery_getter.get_repo_invoker("Files")
 
     try:
         file = files_repo.get({'id': file_id})
         if not file:
-             response_formatter.error('File missing.', 400)
+            response_formatter.error('File missing.', 400)
 
-        file_path = file.get('path')
+        # TO-DO: HardCoded directory path needs to change.
+        file_path = "/Users/tvs/projects/tvs-enterprise/trumpet_be_cloud_platform/" + file.get('path')
 
-
-        return FileResponse(file_path) # type: ignore
-        # with open(file_path, 'r') as file:
-        #     content = file.read()
-        #     print('content', content)
-        # return response_formatter.success( 'data', 'User token verify successfully.', 200)
-
-
-
+        return FileResponse(file_path)  # type: ignore
 
     except Exception as e:
-        print('Error:::',e)
+        print('Error:::', e)
         return response_formatter.error(str(e), 500)
