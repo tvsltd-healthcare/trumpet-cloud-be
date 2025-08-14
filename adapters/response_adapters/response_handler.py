debff51350d7db5fc7ec18ef7f080c87f80ccf39
nayeem <nayeem.hasanewu@gmail.com>
2025-08-14 15:52:03 +0600

    fix: enhance error handling for duplicate host organizations

--- adapters/response_adapters/response_handler.py
+++ adapters/response_adapters/response_handler.py
@@ -53,12 +53,16 @@ class ResponseHandler(IResponseHandler):
             # Possible sollution
             # data_obj = data.get("data", data)
             # response["data"] = data_obj
-            response["data"] = data if _data is None else _data
+            if data.get("status_code") is None or data['status_code'] < 400:
+                response["data"] = data if _data is None else _data
 
         # if _status_code is not None and _status_code >= 400:
         #     raise Exception(f"{_message} with status code {_status_code}")
 
         if errors is not None:
             response["errors"] = errors
+
+        if data.get("errors") is not None:
+            response["errors"] = data["errors"]
 
         return response
