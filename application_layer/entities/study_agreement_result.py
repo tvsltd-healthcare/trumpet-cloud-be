ed6abdf8c46d153e72facc7b85d0df0cbf19cac9
Sakib Nazmus <nsakib.cse@gmail.com>
2025-08-19 15:09:02 +0600

    fix: remove redundant 'required' validation from study_agreement_result entity

--- application_layer/entities/study_agreement_result.py
+++ application_layer/entities/study_agreement_result.py
@@ -16,10 +16,6 @@ StudyAgreementResults = {
             'value': 'text',
             'error_code': '145',
         },
-        'required': {
-            'value': 'true',
-            'error_code': '146',
-        },
     },
     'file_path': {
         'type': {
@@ -31,10 +27,6 @@ StudyAgreementResults = {
         'type': {
             'value': 'string',
             'error_code': '147',
-        },
-        'required': {
-            'value': 'true',
-            'error_code': '148',
         },
         'max': {
             'value': 20,
