7c9880794905469cec77f73c2da7d4739ac8d624
Sakib Nazmus <nsakib.cse@gmail.com>
2025-11-02 22:48:28 +0600

    fix: allow upload file only allowed types.

--- domain_layer/utils/file_upload.py
+++ domain_layer/utils/file_upload.py
@@ -7,6 +7,24 @@ from domain_layer.response_formatter import ResponseFormatter
 # Configuration constants
 OUTPUT_DIR = os.getenv('FILE_UPLOAD_ABS_DIR')
 
+ALLOWED_DOCUMENT_TYPES = [
+    # Images
+    "image/jpeg",  # .jpg, .jpeg
+    "image/png",   # .png
+
+    # Documents
+    "application/pdf",                                                         # .pdf
+    "application/msword",                                                      # .doc (and sometimes .docx)
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document", # .docx
+
+    # Spreadsheets
+    "application/vnd.ms-excel",                                          # .xls (and sometimes .xlsx)
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", # .xlsx
+
+    # Presentations
+    "application/vnd.ms-powerpoint",                                             # .ppt (and sometimes .pptx)
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation", # .pptx
+]
 
 def upload_file_to_disk(file) -> dict:
     """
@@ -23,9 +41,12 @@ def upload_file_to_disk(file) -> dict:
         IOError: If file operations fail (e.g., disk full, permissions).
     """
     response_formatter = ResponseFormatter()
-    # Validate file input
+
     if not file or not getattr(file, 'filename', None):
-        return response_formatter.error('Invalid or missing file.', 400)
+        raise ValueError('Invalid file content.')
+
+    if file.content_type not in ALLOWED_DOCUMENT_TYPES:
+        raise ValueError('Invalid file type.')
 
     # Create output directory
     file_dir = Path(OUTPUT_DIR).absolute()
